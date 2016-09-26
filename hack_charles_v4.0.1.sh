c4847621bd0ca6d226cbc186f62c450f79a6cd8f
york yuan <superitkang@gmail.com>
2016-09-26 15:32:49 +0800

    hack charkes

---
+++ hack_charles_v4.0.1.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+charles=/Applications/Charles.app/Contents/Java/charles.jar
+dir=charleshack
+mkdir $dir
+cd $dir
+cat >> License.java <<EOF
+package com.xk72.charles;
+public final class License {
+    public static boolean a() { return true; }
+    public static String b() { return "itkang"; }
+    public static String a(String name, String key) { return null; }
+}
+EOF
+javac -encoding UTF-8 License.java -d .&& jar -uvf $charles com/xk72/charles/License.class
+cd .. && rm -rf $dir
